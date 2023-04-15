e47b8bac3e81abc5adb96502e2215173c6cd35fe
basiacarvalho <basiacarvalho@gmail.com>
2023-04-15 16:05:02 +0200

    additional feature in the 1st task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,12 +6,16 @@ function createGalleryItems() {
   for (const item of galleryItems) {
     const li = document.createElement('li');
     li.classList.add('gallery__item');
+    const link = document.createElement('a');
+    link.classList.add('gallery__link');
+    link.href = item.original;
     const img = document.createElement('img');
     img.src = item.preview;
     img.alt = item.description;
     img.dataset.originalPicture = item.original;
     img.classList.add('gallery__image');
-    li.append(img);
+    li.append(link);
+    link.append(img);
     items.push(li);
   }
   document.querySelector('.gallery').append(...items);
@@ -22,15 +26,26 @@ createGalleryItems();
 
 const gallery = document.querySelector('.gallery');
 
+let instance;
+
+function closeModal(event) {
+  if (event.code !== 'Escape') {
+    return;
+  }
+  instance.close();
+}
+
 function showPicture(event) {
+  event.preventDefault();
   if (event.target.nodeName !== 'IMG') {
     return;
   }
 
-  const instance = basicLightbox.create(`
+  instance = basicLightbox.create(`
     <img src="${event.target.dataset.originalPicture}" alt="${event.target.alt}">
 `);
 
+  document.addEventListener('keydown', closeModal);
   instance.show();
 }
 
